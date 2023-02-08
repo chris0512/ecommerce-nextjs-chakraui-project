825aa51c4293babafc2e0543c24019d127796743
Chris <kyang3200@gmail.com>
2023-02-09 00:09:05 +0800

    Add image, price, and rating

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,10 +1,97 @@
-import React from 'react';
+import {
+    Flex,
+    Circle,
+    Image,
+    Badge,
+    useColorModeValue,
+    Icon,
+    Button,
+    Tooltip,
+    Stack,
+    Link,
+    HStack,
+    Text, useColorMode,
+    Box,
+} from "@chakra-ui/react";
 
-const ProductCard = () => {
+import { FiShoppingCart } from "react-icons/fi";
+import { Link as ReactLink } from "react-router-dom";
+import { StarIcon } from "@chakra-ui/icons";
+import { useState } from "react";
+
+const ProductCard = ({ product }) => {
     return (
-        <div>
+        <Stack
+            p='2'
+            spacing='3px'
+            bg={useColorModeValue('white', 'gray.800')}
+            minW='240px'
+            h='450px'
+            borderWidth='1px'
+            rounded='lg'
+            shadow='lg'
+            position='relative'
+        >
+            {product.isNew && <Circle
+                                size='10px'
+                                position='absolute'
+                                top='2'
+                                right={2}
+                                bg='green.300'
+                                />
+            }
+            {product.stock <= 0 && <Circle
+                size='10px'
+                position='absolute'
+                top='2'
+                right={2}
+                bg='red.300'
+            />
+            }
+            <Image src={product.image} alt={product.name} roundedTop='lg'/>
 
-        </div>
+            <Box flex='1' maxH='5' alignItems='baseline'>
+                {product.stock <= 0 && (
+                    <Badge rounded='full' px='2' fontSize='0.8em' colorScheme='red'>
+                        Sold out
+                    </Badge>)
+                }
+                {product.isNew && (
+                    <Badge rounded='full' px='2' fontSize='0.8em' colorScheme='green'>
+                        New
+                    </Badge>)
+                }
+            </Box>
+
+            <Flex mt='1' justifyContent='space-between' alignContent='center'>
+                <Link as={ReactLink} to={`/product${product._id}`} pt='2' cursor='pointer'>
+                    <Box fontSize='2xl' fontWeight='semibold' as='h4' lineHeight='tight'>
+                        {product.name}
+                    </Box>
+                </Link>
+            </Flex>
+            <Flex justifyContent='space-between' alignContent='center' py='2'>
+                rating
+            </Flex>
+            <Flex justify='space-between'>
+                <Box fontSize='2xl' color={useColorModeValue('gray.800', 'white')}>
+                    <Box as='span' color={'gray.600'} fontSize='lg'>
+                        $
+                    </Box>
+                    {product.price.toFixed(2)}
+                </Box>
+                <Tooltip
+                    label='Add to cart'
+                    bg='white'
+                    placement='top'
+                    color='gray.800'
+                    fontSize='1.2em'>
+                    <Button variant='ghost' display='flex' disabled={product.stock <= 0}>
+                        <Icon as={FiShoppingCart} h={7} w={7} alignSelf='center'></Icon>
+                    </Button>
+                 </Tooltip>
+            </Flex>
+         </Stack>
     )
 }
 
--- client/src/screens/ProductsScreen.jsx
+++ client/src/screens/ProductsScreen.jsx
@@ -1,6 +1,7 @@
 import { Center, Wrap, WrapItem } from '@chakra-ui/react';
 
 import { products } from '../products';
+import ProductCard from "../components/ProductCard";
 
 
 const ProductsScreen = () => {
@@ -9,7 +10,7 @@ const ProductsScreen = () => {
             {products.map(product => (
                 <WrapItem key={product._id}>
                     <Center w='250px' h='550px'>
-                        { product.name}
+                        <ProductCard product={product}></ProductCard>
                     </Center>
                 </WrapItem>
             ))}
