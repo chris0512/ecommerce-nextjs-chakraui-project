d645486fbef99e01c7f5fc15aafa5189ddd00d64
Chris <kyang3200@gmail.com>
2023-02-08 22:11:28 +0800

    add image file and products.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { ChakraBaseProvider} from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import ProductsScreen from "./screens/ProductsScreen";
 
 
 function App() {
@@ -8,6 +9,12 @@ function App() {
     <ChakraBaseProvider>
       <Router>
         <Navbar />
+          <main>
+              <Routes>
+                  <Route path="/products" element={<ProductsScreen/>}></Route>
+              </Routes>
+
+          </main>
       </Router>
     </ChakraBaseProvider>
   );
