1e86b53936ecc68f9ee4e81f36134e791e2d4131
Chris <kyang3200@gmail.com>
2023-02-09 14:44:27 +0800

    link tht router /api/products

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ import dotenv from 'dotenv';
 import connectToDatabase from "./database.js";
 import express from "express";
 
+// Our routes
+import ProductRoutes from "./routes/productRoutes.js";
+
+
+
 dotenv.config();
 connectToDatabase();
 
@@ -10,6 +15,9 @@ const app = express();
 app.use(express.json());
 
 const port = process.env.PORT || 5001;
+
+app.use('/api/products', ProductRoutes);
+
 
 app.listen(port, () => {
     console.log(`Server runs on port ${port}.`);
